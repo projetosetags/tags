ee173a376354bb464a32738b94df4bec2aa8b7a7
dashboard-sedam <68096665+projetosetags@users.noreply.github.com>
2026-07-15 10:17:44 -0300

    Remove Supabase client initialization
    Removed Supabase client initialization from config.js.

--- config.js
+++ config.js
@@ -1,15 +1,6 @@
 window.S_URL='https://zvtzbiqfwhggysiuiuxh.supabase.co'
 
 window.S_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2dHpiaXFmd2hnZ3lzaXVpdXhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1ODYyMjcsImV4cCI6MjA5MzE2MjIyN30.tCnFZv2B6Kmd9KsyZM8CHClZqsq7Nyu-8nxyYVs_ZMQ'
-window.client = supabase.createClient(
-    window.S_URL,
-    window.S_KEY,
-    {
-        db:{
-            schema:'queimadas'
-        }
-    }
-)
 
 console.log('SUPABASE OK')
 console.log(window.client)
