ce311843e8fc6ec2adc771c8a5f569a473548727
dashboard-sedam <68096665+projetosetags@users.noreply.github.com>
2026-07-15 00:49:48 -0300

    Add service worker with caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='queimadas-v1'
+
+self.addEventListener('install',e=>{
+self.skipWaiting()
+})
+
+self.addEventListener('activate',e=>{
+clients.claim()
+})
+
+self.addEventListener('fetch',e=>{
+e.respondWith(fetch(e.request))
+})
