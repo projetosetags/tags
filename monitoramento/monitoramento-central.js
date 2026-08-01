3a4fe88ef6c3855448e3ebe8d124e62883956d34
dashboard-sedam <68096665+projetosetags@users.noreply.github.com>
2026-08-01 00:44:34 -0300

    Add header comment to monitoramento-central.js
    Add header comment for monitoramento-central.js

--- monitoramento/monitoramento-central.js
+++ monitoramento/monitoramento-central.js
@@ -1,3 +1,8 @@
+/*=========================================================
+000 MONITORAMENTO CLIENT
+=========================================================*/
+const client = window.clientPublic
+  
 async function carregarCentralEvidencias(){
 
 let{data,error}=await client
