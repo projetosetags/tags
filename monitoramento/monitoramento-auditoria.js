a6b9b5fc8d7dc6e4fff27b5ed767432dadd793a8
dashboard-sedam <68096665+projetosetags@users.noreply.github.com>
2026-08-01 00:43:51 -0300

    Add header comment and client initialization
    Added header comment and initialized client variable.

--- monitoramento/monitoramento-auditoria.js
+++ monitoramento/monitoramento-auditoria.js
@@ -1,3 +1,8 @@
+/*=========================================================
+000 MONITORAMENTO CLIENT
+=========================================================*/
+const client = window.clientPublic
+
 async function carregarAuditoriaCompleta(){
 
 let{data,error}=await client
