8df5adf035bd7ff11a52ec47ded6ad08f60af4d6
dashboard-sedam <68096665+projetosetags@users.noreply.github.com>
2026-09-06 18:32:22 -0300

    Carrega nova linha do tempo com Data e Mes nas primeiras colunas

--- js/queimadas-03fontes.js
+++ js/queimadas-03fontes.js
@@ -25,7 +25,9 @@ carregar('../js/queimadas-relatorio-municipios-v2.js?v=20260906-1','qmtRelatorio
 carregar('../js/queimadas-relatorio-municipios-v3.js?v=20260906-1','qmtRelatorioMunicipiosV3',function(){
 carregar('../js/queimadas-municipios-timeline-horizontal-v6.js?v=20260906-1','qmtTimelineHorizontalV6',function(){
 carregar('../js/queimadas-municipios-timeline-vertical-v7.js?v=20260906-1','qmtTimelineVerticalV7',function(){
-carregar('../js/queimadas-municipios-timeline-ordem-v8.js?v=20260906-1','qmtTimelineOrdemV8')
+carregar('../js/queimadas-municipios-timeline-ordem-v8.js?v=20260906-1','qmtTimelineOrdemV8',function(){
+carregar('../js/queimadas-municipios-timeline-v8.js?v=20260906-2','qmtTimelineV8')
+})
 })
 })
 })
